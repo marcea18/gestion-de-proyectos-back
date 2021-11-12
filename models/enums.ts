0dcea09a60f05a96c89ff59cb86a5b0a62283e42
Marcela Aristizabal <marcea18@gmail.com>
2021-11-12 06:26:10 -0500

    agregar relaciones one to many de user a project y de project a objective

--- models/enums.ts
+++ models/enums.ts
@@ -1,7 +1,30 @@
 enum Enum_Rol {
-    estudiante = 'Estudiante',
-    lider = 'Líder',
-    administrador = 'Administrador',
-  }
-  
-  export { Enum_Rol };
+  estudiante = 'Estudiante',
+  lider = 'Líder',
+  administrador = 'Administrador',
+}
+
+enum Enum_EstadoUsuario {
+  pendiente = 'Pendiente',
+  autorizado = 'Autorizado',
+  no_autorizado = 'No Autorizado',
+}
+
+enum Enum_EstadoProyecto {
+  activo = 'Activo',
+  inactivo = 'Inactivo',
+}
+
+enum Enum_FaseProyecto {
+  iniciado = 'Iniciado',
+  desarrollo = 'En Desarrollo',
+  terminado = 'Terminado',
+  nula = '',
+}
+
+enum Enum_TipoObjetivo {
+  general = 'General',
+  especifico = 'Especifico',
+}
+
+export { Enum_Rol, Enum_EstadoUsuario, Enum_EstadoProyecto, Enum_FaseProyecto, Enum_TipoObjetivo };
